4a4c73babba10646f7271950d2189eb1b6649ec5
pythonpete32 <aaron@blackscale.io>
2023-10-12 11:58:49 +0400

    test: update base test with

--- test/base/TribeTest.sol
+++ test/base/TribeTest.sol
@@ -1,9 +1,32 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.17;
 
+import { IPluginSetup, PluginSetup } from "@aragon/osx/framework/plugin/setup/PluginSetup.sol";
+import { DAO } from "@aragon/osx/core/dao/DAO.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
 import { Test } from "forge-std/Test.sol";
 
 contract TribeTest is Test {
+    bytes internal EMPTY_BYTES;
+
+    function _createDaoWithPlugin(IPluginSetup setup) internal returns (DAO, address) {
+        // create dao
+        DAO _dao = DAO(payable(new ERC1967Proxy(address(new DAO()), EMPTY_BYTES)));
+
+        // initialize the DAO and give the `ROOT_PERMISSION_ID` permission to this contract.
+        _dao.initialize(EMPTY_BYTES, address(this), address(0), "");
+
+        // deploy plugin
+        (address plugin, PluginSetup.PreparedSetupData memory preparedSetupData) =
+            setup.prepareInstallation(address(_dao), EMPTY_BYTES);
+
+        // install plugin
+        _dao.applyMultiTargetPermissions(preparedSetupData.permissions);
+
+        return (_dao, plugin);
+    }
+
     function account(string memory label) internal returns (address addr) {
         (addr,) = accountAndKey(label);
     }
