9b7542498ea1465c662f3a6184a1f62cf6b3776f
pythonpete32 <aaron@blackscale.io>
2023-11-20 20:20:27 +0300

    feat(hooks): add  hook

---
+++ packages/hooks/src/hooks/useIsMember.ts
@@ -0,0 +1,30 @@
+import { useAccount } from "wagmi";
+
+import { useStore } from ".";
+import { useQuery } from "@tanstack/react-query";
+import { tokenVotingClient } from "../service/aragon";
+import { TokenVotingMember } from "@aragon/sdk-client";
+
+export function useIsMember() {
+  const { tokenVotingAddress } = useStore();
+  const { address } = useAccount();
+  const { data: isMember, ...rest } = useQuery({
+    queryKey: ["isMember", tokenVotingAddress, address],
+    queryFn: async () =>
+      tokenVotingClient.methods.getMembers({
+        pluginAddress: tokenVotingAddress!,
+      }),
+    enabled: !!(tokenVotingAddress && address),
+    select(data: Array<TokenVotingMember>) {
+      console.log(data);
+      console.log(address);
+      if (address === undefined) return false;
+      return data.some(
+        (member) =>
+          member.address.toLowerCase() === address.toLowerCase() ||
+          member.delegators.some((delegator) => delegator.address.toLowerCase() === address.toLowerCase()),
+      );
+    },
+  });
+  return { isMember, ...rest };
+}
