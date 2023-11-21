0caa88bdf29ab99fb419c3740c581eaffff3c544
pythonpete32 <aaron@blackscale.io>
2023-11-21 10:19:33 +0300

    🧹

--- src/components/RectangleComponent.tsx
+++
@@ -1,9 +0,0 @@
-import { FunctionComponent } from "react";
-
-const RectangleComponent: FunctionComponent = () => {
-  return (
-    <div className="relative bg-gray w-[20.83rem] h-[45.11rem] mt-[-812px]" />
-  );
-};
-
-export default RectangleComponent;
